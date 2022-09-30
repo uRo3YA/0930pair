61ea960afd4529be68c9c7a70b7f9187ed72995d
uRo3YA <ltg8693@gmail.com>
2022-09-30 10:34:31 +0900

    create 구현

--- pair/test0930/urls.py
+++ pair/test0930/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "test0930"
 urlpatterns = [
     path("", views.index, name="index"),
-
+    path("create/", views.create, name="create"),
 ]
--- pair/test0930/views.py
+++ pair/test0930/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
-#from .models import Review
+from django.shortcuts import render, redirect
+from .models import Review
+
 # Create your views here.
 def index(request):
-    #review = Review.objects.all()
-    #context = {"review": review}
-    return render(request, "test0930/index.html")
-    #return render(request, "test0930/index.html", context)
+    review = Review.objects.all()
+    context = {"review": review}
+    # return render(request, "test0930/index.html")
+    return render(request, "test0930/index.html", context)
+
+
+def create(request):
+
+    # 1. parameter로 날라온 데이터를 받아서
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    # 2. DB에 저장
+    Review.objects.create(title=title, content=content)
+
+    context = {
+        "title": title,
+        "content": content,
+    }
+
+    return redirect("test0930:index")
