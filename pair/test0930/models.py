5512a46c896d93c2eac8d2fe4183d52560410177
uRo3YA <ltg8693@gmail.com>
2022-09-30 10:18:39 +0900

    db 마이그레이션

--- pair/test0930/models.py
+++ pair/test0930/models.py
@@ -1,8 +1,9 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
+
 # Create your models here.
-# class Review(models.Model):
-#     title=models.CharField(max_length=80)
-#     content = models.CharField()
-#     created_at = models.DateField(auto_now_add=True)
-#     updated_at= models.DateField(auto_now=True)
+class Review(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
